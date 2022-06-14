e5434fee439ec0b508328bb4cc27c9e3b89ca798
Moulun Florian <flomoulun@gmx.fr>
2022-06-14 11:19:04 +0200

    [ADD]
    Ajout du js

--- hello-elementor-child/functions.php
+++ hello-elementor-child/functions.php
@@ -3,3 +3,18 @@ add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
+
+
+function wpb_hook_javascript() {
+    ?>
+    <script>
+
+        const bouton = document.querySelector("#bouton");
+        bouton.addEventListener("click", function(){
+            console.log("mon js est enfin liée");
+        });
+
+    </script>
+    <?php
+}
+add_action('wp_footer', 'wpb_hook_javascript');
